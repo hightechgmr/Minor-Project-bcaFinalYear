7f13367aae8fd5cab558b50c0c0ecb59f5aa1190
Jatin Pant <pantjatin798@gmail.com>
2026-04-18 00:02:03 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,3 +1,63 @@
+<?php
+session_start();
+
+
+if (!isset($_SESSION['user'])) {
+    header("Location: login.php");
+    exit();
+}
+
+// Database connection
+$conn = mysqli_connect("localhost", "root", "", "tictactoe");
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Logged-in user
+$user = $_SESSION['user'];
+
+
+//  USER DATA 
+$userQuery = "SELECT username, gender FROM users WHERE username='$user'";
+$userResult = mysqli_query($conn, $userQuery);
+
+if ($userResult && mysqli_num_rows($userResult) > 0) {
+    $userData = mysqli_fetch_assoc($userResult);
+    $username = $userData['username'];
+    $gender = $userData['gender'];
+} else {
+    $username = "Not Found";
+    $gender = "Not Found";
+}
+
+
+//SCORE DATA
+$scoreQuery = "
+SELECT 
+    COALESCE(SUM(total_matches),0) AS total_matches,
+    COALESCE(SUM(won),0) AS won,
+    COALESCE(SUM(lost),0) AS lost
+FROM scorecard 
+WHERE user_name='$user'
+";
+
+$scoreResult = mysqli_query($conn, $scoreQuery);
+
+if ($scoreResult) {
+    $scoreData = mysqli_fetch_assoc($scoreResult);
+
+    $total_matches = $scoreData['total_matches'];
+    $won = $scoreData['won'];
+    $lost = $scoreData['lost'];
+} else {
+    $total_matches = 0;
+    $won = 0;
+    $lost = 0;
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,57 +68,52 @@
 </head>
 <body>
     <header>
-      <nav class="navigation">
-        <a href="indexafterlogin.php">Home</a>
-        <a href="rule.php">Game Rules</a>
-        <a href="score.php">Scorecard</a> 
-      </nav>    
-    </header>
-
-    <content class="content">
-        <div class="head2">
-            <div id="profilepic">
-                <img src="images/profilemale.png" alt="profile pic" id="malepic">
+        <div class="border"></div>
+        <div class="navbar">
+            <div class="buttons">
+                <input type="button" id="about" class="button" value="About">
+                <input type="button" id="ruleofgame" class="button" value="Rules of games" style="background-color:#F2E9E4;" onclick="window.location.href='rule.php'">
+                <input type="button" id="scorecard" class="button" value="Score card" style="background-color:#F2E9E4;" onclick="window.location.href='scorecard.php'">
+                <input type="button" id="log-sign" class="button" value="Login/Signup">
+                <img src="images/profile.png" alt="profile" id="profile" onclick="window.location.href='scorecard.php'">
             </div>
-            <div class="headings" style="color: black;
-    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;"> Profile </div>
         </div>
-
-
+    </header>
+    <content>
+        <div class="head">
+            <div id="div-logo"><img src="images/logo.jpeg" alt="logo" id="logo"></div>
+            <div id="topic">Profile:</div>
+            <div id="profilepic"><img src="images/profilemale.png" alt="profile pic" id="malepic"></div>
+        </div>
         <table id="frm-table">
             <tr>
-                <td>Username:</td>
-                <td><input type="text" id="usrnm"></td>
+                <td>User Name:</td>
+                <td><input type="text" value="<?php echo $username; ?>" readonly></td>
             </tr>
             <tr>
                 <td>Gender:</td>
-                <td><input type="text" id="Gender"></td>
+                <td><input type="text" value="<?php echo $gender; ?>" readonly></td>
             </tr>
             <tr>
                 <td>Total Matches:</td>
-                <td><input type="text" id="Totlmatch"></td>
+                <td><input type="text" value="<?php echo $total_matches; ?>" readonly></td>
             </tr>
             <tr>
                 <td>Won:</td>
-                <td><input type="text" id="won"></td>
+                <td><input type="text" value="<?php echo $won; ?>" readonly></td>
             </tr>
             <tr>
                 <td>Lost:</td>
-                <td><input type="text" id="lost"></td>
+                <td><input type="text" value="<?php echo $lost; ?>" readonly></td>
             </tr>
         </table>
-
         <div id="rmv">
-            <input type="button" id="rmvacc" value="Log Out">
+            <input type="button"  id="rmvacc" value="Log Out">
         </div>
             
-        <div id="div-logo">
-                <img src="images/logo.jpeg" alt="logo" id="logo2">
-            </div>
     </content>
-
-    <footer class="fotter">
+    <footer id="footer">
+        <div></div>
     </footer>
-
 </body>
 </html>
