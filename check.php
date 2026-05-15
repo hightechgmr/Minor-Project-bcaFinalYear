b7ae5e3ff01a8a571a450088d6cd159c5b231459
Jatin Pant <pantjatin798@gmail.com>
2026-05-15 22:30:23 +0530

    update

--- check.php
+++ check.php
@@ -21,15 +21,17 @@ if(isset($_POST['login']))
     $password = trim($_POST['password']);
 
   
-    $query = "SELECT * FROM users WHERE BINARY username = ? AND BINARY password = ?";
+    $query = "SELECT username, password FROM users WHERE BINARY username = ?";
     $stmt = mysqli_prepare($conn,$query);
-    mysqli_stmt_bind_param($stmt,"ss",$username,$password);
+    mysqli_stmt_bind_param($stmt,"s",$username);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
+    $row = mysqli_fetch_assoc($result);
 
-    if(mysqli_num_rows($result) == 1)
+    if($row && password_verify($password, $row['password']))
     {
-        $_SESSION['username'] = $username;
+        $_SESSION['user'] = $row['username'];
+        $_SESSION['username'] = $row['username'];
         header("Location: indexafterlogin.php");
         exit();
     }
